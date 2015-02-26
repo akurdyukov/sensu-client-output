d1a0ee5d390a02b4c92c343394d4a766a6248237
Alex Razgul <guliaymsk2@mail.ru>
2015-02-26 10:23:28 -0500

    zabbix ouput plugin implemented

--- src/main/java/org/kurdyukov/sensu/ZabbixServerOutput.java
+++ src/main/java/org/kurdyukov/sensu/ZabbixServerOutput.java
@@ -4,17 +4,21 @@ import com.google.inject.assistedinject.Assisted;
 import org.graylog2.plugin.Message;
 import org.graylog2.plugin.configuration.Configuration;
 import org.graylog2.plugin.configuration.ConfigurationRequest;
-import org.graylog2.plugin.configuration.fields.BooleanField;
 import org.graylog2.plugin.configuration.fields.ConfigurationField;
 import org.graylog2.plugin.configuration.fields.NumberField;
 import org.graylog2.plugin.configuration.fields.TextField;
 import org.graylog2.plugin.outputs.MessageOutput;
-import org.graylog2.plugin.outputs.MessageOutputConfigurationException;
 import org.graylog2.plugin.streams.Stream;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -28,20 +32,47 @@ public class ZabbixServerOutput implements MessageOutput {
     private final static String CK_ZABBIX_PORT = "zabbix_port";
     private final static String CK_KEY_PATTERN = "zabbix_key_pattern";
     private final static String CK_VALUE_PATTERN = "zabbix_value_pattern";
+    private final static String REQUEST_FIELD_VALUE = "ZBXD\\x01";
+    private final static String ZABBIX_HEADER = "sender data";
 
     private final AtomicBoolean isRunning = new AtomicBoolean(false);
-
+    private Socket socket;
+    private Configuration configuration;
+    private OutputStream stream;
     @Inject
-    public ZabbixServerOutput(@Assisted Configuration configuration) throws MessageOutputConfigurationException {
+    public ZabbixServerOutput(@Assisted Configuration configuration) throws Exception {
+        this.configuration = configuration;
         LOG.info("Initializing");
-        // TODO: open socket
+
+        initializeSocket();
         isRunning.set(true);
     }
+    private void initializeSocket() throws Exception {
 
+        if(socket != null && socket.isConnected()){
+            socket.close();
+        }
+
+        socket = new Socket(configuration.getString(CK_ZABBIX_HOST), configuration.getInt(CK_ZABBIX_PORT));
+        LOG.info("connecting to zabbix server...");
+        socket.connect(socket.getRemoteSocketAddress(), 5000);
+
+        if(socket.isConnected()){
+            LOG.info("connection to zabbix server has been established");
+           stream = socket.getOutputStream();
+        }else{
+            throw new Exception("failed to connect to zabbix server");
+        }
+    }
     @Override
     public void stop() {
-        // TODO: close socket
+        try {
+            socket.close();
+        } catch (IOException e) {
+            LOG.error("Problems stopping socket", e);
+        }
         isRunning.set(false);
+        LOG.info("Stopped");
     }
 
     @Override
@@ -51,11 +82,56 @@ public class ZabbixServerOutput implements MessageOutput {
 
     @Override
     public void write(Message message) throws Exception {
-        // TODO: implement writing
+
+        JSONObject jsonData = new JSONObject();
+        jsonData.put("host",configuration.getString(CK_ZABBIX_HOST));
+
+
+        String keyString = GetFormattedResult(message, configuration.getString(CK_KEY_PATTERN));
+        String valueString = GetFormattedResult(message, configuration.getString(CK_VALUE_PATTERN));
+
+        jsonData.put("key", keyString);
+        jsonData.put("value", valueString);
+
+        JSONObject json = new JSONObject();
+        json.put("request", REQUEST_FIELD_VALUE);
+        json.put("data", Arrays.asList(jsonData.toString()));
+
+        byte[] data = json.toString().getBytes();
+        byte[] header = REQUEST_FIELD_VALUE.getBytes();
+
+        ByteBuffer buffer = ByteBuffer.allocate(header.length + 4 + data.length);
+
+        int currentOffset = 0;
+
+        buffer.put(header, 0, header.length);
+        currentOffset += header.length;
+
+        buffer.putInt(currentOffset, data.length);
+        currentOffset += 4;
+
+        buffer.put(data, currentOffset, data.length);
+
+        stream.write(buffer.array());
+    }
+
+    private String GetFormattedResult(Message message, String pattern) {
+        String result = new String(pattern);
+
+        for (String fieldName : message.getFieldNames()){
+            result = result.replace(String.format("%({0})", fieldName), message.getField(fieldName).toString());
+        }
+
+        return result;
     }
 
     @Override
     public void write(List<Message> messages) throws Exception {
+
+        if(socket == null || !socket.isConnected()){
+            initializeSocket();
+        }
+
         for (Message msg : messages) {
             write(msg);
         }
@@ -101,7 +177,7 @@ public class ZabbixServerOutput implements MessageOutput {
             c.addField(new TextField(
                     CK_KEY_PATTERN,
                     "Trapper key pattern",
-                    "",
+                    "%(server).%(level)",
                     "Trapper key pattern",
                     ConfigurationField.Optional.NOT_OPTIONAL
             ));
@@ -109,7 +185,7 @@ public class ZabbixServerOutput implements MessageOutput {
             c.addField(new TextField(
                     CK_VALUE_PATTERN,
                     "Trapper value pattern",
-                    "",
+                    "%(message).%(exception)",
                     "Trapper value pattern",
                     ConfigurationField.Optional.NOT_OPTIONAL
             ));
